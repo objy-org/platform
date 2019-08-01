5bdd060b4ed92540b0a7a4d4613755523d744252
Marco Boelling <53232140+4ty711@users.noreply.github.com>
2019-08-01 09:16:26 +0200

    Update spoo.js

--- spoo.js
+++ spoo.js
@@ -933,13 +933,13 @@ var SPOO = {
 
         if(self.caches[templateRole || obj.role].get(templateId))
         {
-            //run(self.caches[templateRole || obj.role].get(templateId))
+            run(self.caches[templateRole || obj.role].get(templateId))
 
         } else {
 
                SPOO[templateRole || obj.role](templateId).get(function(template){
 
-                    //if(!self.caches[templateRole || obj.role].get(templateId)) self.caches[templateRole || obj.role].add(templateId,  template);
+                    if(!self.caches[templateRole || obj.role].get(templateId)) self.caches[templateRole || obj.role].add(templateId,  template);
                     
                     run(template)
 
