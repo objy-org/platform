8f5cbbb43f3fa5675acc5bd0277bf1e06fe98a32
Marco Boelling <53232140+4ty711@users.noreply.github.com>
2019-08-08 09:43:51 +0200

    changed bull vm mapper

--- catalog/processors/vmMapperBull.js
+++ catalog/processors/vmMapperBull.js
@@ -1,51 +1,60 @@
-const Mapper = require('./_template.js');
+var Global = require('./_template.js');
 const { VM, VMScript } = require('vm2');
-var SPOO = require('./spoo.js');
 var Queue = require('bull');
 
 
-Mapper.sandBox = new VM({ sandbox: { SPOO: this.SPOO, dsl: this, this: this } });
+Mapper = function(SPOO) {
+    return Object.assign(new Global(SPOO), {
 
-Mapper.jobQueue = new Queue('spoo jobs', { redis: rediscon });
+        sandBox: new VM({ sandbox: { SPOO: this.SPOO, dsl: this, this: this } }),
+        jobQueue: new Queue('spoo jobs', 'redis://127.0.0.1:6379'),
 
-Mapper.jobQueue.process(function(job, done) {
-    new Mapper(SPOO).executeFromJob(job.data.dsl, JSON.parse(job.data.obj), JSON.parse(job.prop || {}), job.data.data, );
-});
+        init: function() {
+            this.jobQueue.process(function(job, done) {
+                console.info('executing...')
+                new Mapper(SPOO).executeFromJob(job.data.dsl, JSON.parse(job.data.obj), JSON.parse(job.prop || {}), job.data.data, );
+            });
 
-Mapper.jobQueue.on('completed', function(job, result) {
-    job.remove();
-})
+            this.jobQueue.on('completed', function(job, result) {
+                job.remove();
+            })
+        },
 
-// execute from original caller
-Mapper.execute = function(dsl, obj, prop, data, callback, client, app, options) {
+        execute: function(dsl, obj, prop, data, callback, client, app, options) {
 
-    if (this.multitenancy == this.CONSTANTS.MULTITENANCY.ISOLATED) {
-        this.jobQueue.add({
-            dsl: dsl,
-            obj: JSON.stringify(obj || {}),
-            prop: JSON.pringify(prop || {}),
-            data: JSON.stringify(data || {}),
-            client: client,
-            app: app,
-            options: JSON.stringify(options || {})
-        });
-    } else {
-        this.jobQueue.add({
-            dsl: dsl,
-            obj: JSON.stringify(obj || {}),
-            prop: JSON.pringify(prop || {}),
-            data: JSON.stringify(data || {}),
-            client: client,
-            app: app,
-            options: JSON.stringify(options || {})
-        });
-    }
+            if (this.multitenancy == this.CONSTANTS.MULTITENANCY.ISOLATED) {
+
+                this.jobQueue.add({
+                    dsl: dsl,
+                    obj: JSON.stringify(obj || {}),
+                    prop: JSON.stringify(prop || {}),
+                    data: JSON.stringify(data || {}),
+                    client: client,
+                    app: app,
+                    options: JSON.stringify(options || {})
+                });
+            } else {
+                this.jobQueue.add({
+                    dsl: dsl,
+                    obj: JSON.stringify(obj || {}),
+                    prop: JSON.stringify(prop || {}),
+                    data: JSON.stringify(data || {}),
+                    client: client,
+                    app: app,
+                    options: JSON.stringify(options || {})
+                });
+            }
+
+            callback();
+
+        },
+
+
+
+        executeFromJob: function(dsl, obj, prop, data, callback, client, app, options) {
+            this.sandBox.run(new VMScript(dsl));
+        }
+    })
 }
-
-// execute single job
-Mapper.executeFromJob = function(dsl, obj, prop, data, callback, client, app, options) {
-    this.sandBox.run(new VMScript(dsl));
-}
-
 
 module.exports = Mapper;
