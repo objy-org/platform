3708e238ad24a4895b5d3f926db046f366597521
Marco Boelling <53232140+4ty711@users.noreply.github.com>
2020-02-25 08:48:12 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,12 +1,26 @@
-var SPOO = require('spoo');
+var SPOO = require('./index.js');
+var Mongo = require('./node_modules/@spootechnologies/objy/mappers/storage/mongoMapper.js');
 
-SPOO.define({
-    name: "User",
-    pluralName: "Users",
-    storage: null
+SPOO.metaPropPrefix = '_';
+
+
+var MetaMapper = new SPOO.metaMappers.mongoMapper().connect('mongodb://localhost', function(data) {
+    console.info('meta connected')
+}, function(data) {
+    console.info('meta not connected', data)
 })
 
+SPOO.define({
+    name: "user",
+    pluralName: "users",
+    storage: new Mongo(SPOO.OBJY).connect('mongodb://localhost', function(data) {
+        console.info('mongo connected')
+    }, function(data) {
+        console.info('mongo not connected', data)
+    }),
+})
 
 SPOO.REST({
+    metaMapper: MetaMapper,
     port: 80
 });
